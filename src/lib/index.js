ed91ddc24853e9333dd43b48d718efef5da980ef
Wisnu Wijokangko <wisnuwijo33@gmail.com>
2021-07-06 12:07:56 +0700

    add template2 to export

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,9 +3,11 @@ import MenuList from './component/menuList';
 import SubMenu from './component/submenu';
 import MiniSidebar from './component/miniSidebar';
 import Template1 from "./component/template1";
+import Template2 from "./component/template2";
 
 export {
     Template1,
+    Template2,
     Menu,
     MenuList,
     SubMenu,
